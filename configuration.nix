917b33a2bac3b0391b3b4406de33e019291a9134
Thomas <thomas@petersson.priv.no>
2025-03-24 20:40:11 +0100

    lol symlinked

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,221 @@
-/home/thomas/nixconfig
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, lib, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      <home-manager/nixos>
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot = {
+    enable = true;
+    windows = {
+      "windows" = let boot-drive = "FS1";
+        in {
+          title = "Windows";
+          efiDeviceHandle = boot-drive;
+          sortKey = "0_windows";
+        };
+    };
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # boot.loader = {
+  #   grub = {
+  #  enable = true;
+  #  devices = ["nodev"];
+  #  efiSupport = true;
+  #  useOSProber = true;
+  #   };
+  # };
+  boot.supportedFilesystems = [ "ntfs" ];
+
+
+  networking.hostName = "thomas"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nb_NO.UTF-8";
+    LC_IDENTIFICATION = "nb_NO.UTF-8";
+    LC_MEASUREMENT = "nb_NO.UTF-8";
+    LC_MONETARY = "nb_NO.UTF-8";
+    LC_NAME = "nb_NO.UTF-8";
+    LC_NUMERIC = "nb_NO.UTF-8";
+    LC_PAPER = "nb_NO.UTF-8";
+    LC_TELEPHONE = "nb_NO.UTF-8";
+    LC_TIME = "nb_NO.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+  hardware.nvidia = {
+  	package = config.boot.kernelPackages.nvidiaPackages.stable;
+          # This will no longer be necessary when
+          # https://github.com/NixOS/nixpkgs/pull/326369 hits stable
+          modesetting.enable = lib.mkDefault true;
+          # Power management is nearly always required to get nvidia GPUs to
+          # behave on suspend, due to firmware bugs.
+          powerManagement.enable = true;
+          powerManagement.finegrained = false;
+          # The open driver is recommended by nvidia now, see
+          # https://download.nvidia.com/XFree86/Linux-x86_64/565.77/README/kernel_open.html
+          open = false;
+	  nvidiaSettings = true;
+  };
+
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_xanmod;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "no";
+    variant = "nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "no";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  programs.zsh.enable = true;
+  users.users.thomas = {
+    isNormalUser = true;
+    home = "/home/thomas";
+    shell = pkgs.zsh;
+    description = "Thomas";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  fonts.packages = with pkgs; [ fira-code ];
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    home-manager
+
+  # core 
+    file
+    fzf
+    git
+    htop
+    ripgrep
+    unzip
+    wget
+    xclip
+    zip
+    wl-clipboard
+    gcc
+    clang
+    gnumake
+
+  # development
+    git
+    blender
+    neovim
+    wezterm
+
+  # leasure
+    thunderbird
+    discord-ptb
+    spotify
+  ];
+
+  programs.noisetorch.enable = true;
+
+  environment.variables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    BROWSER = "firefox";
+    TERMINAL = "wezterm";
+  };
+
+  # Steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
